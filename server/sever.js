03932cf815d1742e9dce40968d60ad23587d9cb3
KanchanKamlu <kanchankamlu2004@gmail.com>
2025-01-28 19:58:47 +0530

    eleven

--- server/sever.js
+++ server/sever.js
@@ -6,33 +6,32 @@ import connectDB from './config/db.js'
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from './controllers/webhooks.js'
 
-const app = express();
+// Initialise express
 
-Sentry.setupExpressErrorHandler(app); // Ensure Sentry setup early
+const app = express()
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+// connect to database
+await connectDB()
+
+// Middlewares
+
+app.use(cors())
+app.use(express.json())
 
 // Routes
-app.get('/', (req, res) => res.send("API Working Successfully"));
-app.get('/debug-sentry', (req, res) => {
-  throw new Error("My first Sentry error!");
-});
-app.post('/webhooks', clerkWebhooks);
+app.get('/',(req,res)=> res.send("API Working Successfully"))/
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
 
-const PORT = process.env.PORT || 5000;
+app.post('/webhooks',clerkWebhooks)
+  
 
-// Start server after DB connection
-const startServer = async () => {
-  try {
-    await connectDB();
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error('Failed to connect to the database', error);
-  }
-};
+// Port 
+const PORT = process.env.PORT || 5000
 
-startServer();
+Sentry.setupExpressErrorHandler(app);
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
