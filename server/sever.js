df66110c3f870dc1c02f64c250dc387b52dc6f9a
KanchanKamlu <kanchankamlu2004@gmail.com>
2025-01-28 22:15:11 +0530

    commit_11

--- server/sever.js
+++ server/sever.js
@@ -3,7 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
-import * as Sentry from "@sentry/node";
+//import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from './controllers/webhooks.js'
 
 // Initialise express
@@ -20,9 +20,9 @@ app.use(express.json())
 
 // Routes
 app.get('/',(req,res)=> res.send("API Working Successfully"))/
-app.get("/debug-sentry", function mainHandler(req, res) {
-    throw new Error("My first Sentry error!");
-  });
+// app.get("/debug-sentry", function mainHandler(req, res) {
+//     throw new Error("My first Sentry error!");
+//   });
 
 app.post('/webhooks',clerkWebhooks)
   
@@ -30,7 +30,7 @@ app.post('/webhooks',clerkWebhooks)
 // Port 
 const PORT = process.env.PORT || 5000
 
-Sentry.setupExpressErrorHandler(app);
+//Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
